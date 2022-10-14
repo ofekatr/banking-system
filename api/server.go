5059cb9841e34bff78a9307700754f7fc996510f
Ofek Atar <ofek.atar@snyk.io>
2022-10-14 17:52:17 +0300

    feat: Added get account endpoint

--- api/server.go
+++ api/server.go
@@ -20,6 +20,8 @@ func NewServer(store *db.Store) *Server {
 
 	router.POST("/accounts", server.createAccount)
 
+	router.GET("/accounts/:id", server.getAccount)
+
 	server.router = router
 
 	return server
